a9a2857130ac8dedfdeece3717556abec0538f18
Denis <den.sautner@mail.ru>
2024-02-21 13:20:39 +0600

    feat: add components to the course-page

---
+++ src/pages/course/Course.tsx
@@ -0,0 +1,50 @@
+import { Header } from 'components'
+import { Button } from 'components/UI/Button/Button'
+import { ProgressBar } from 'components/ProgressBar/ProgressBar'
+import videoPoster from './img/videoPoster.png'
+import videoButtonPlay from './img/videoButtonPlay.svg'
+import styles from './Course.module.scss'
+
+export const Course = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Header />
+        <h1 className={styles.title}>Йога</h1>
+        <p className={styles.heading}>Красота и здоровье / Йога на каждый день / 2 день</p>
+        <div className={styles.videoWrapper}>
+          <img className={styles.videoButton} src={videoButtonPlay} />
+          <video className={styles.video} poster={videoPoster} />
+        </div>
+        <div className={styles.description}>
+          <div className={styles.tasks}>
+            <p className={styles.heading}>Упражнения</p>
+            <ul className={styles.tasksList}>
+              <li className={styles.tasksListItem}>Наклон вперед (10 повторений)</li>
+              <li className={styles.tasksListItem}>Наклон назад (10 повторений)</li>
+              <li className={styles.tasksListItem}>Поднятие ног, согнутых в коленях (5 повторений)</li>
+            </ul>
+            <Button variant="base" children={'Заполнить свой прогресс'} fontSize={18}/>
+          </div>
+          <div className={styles.progress}>
+            <p className={styles.heading}>Мой прогресс по тренировке 2:</p>
+            <div className={styles.progressItems}>
+              <div className={styles.progressItem}>
+                <p className={styles.progressItemText}>Наклоны вперед</p>
+                <ProgressBar maxValue={100} currentValue={45} variant={'blue'} />
+              </div>
+              <div className={styles.progressItem}>
+                <p className={styles.progressItemText}>Наклоны назад</p>
+                <ProgressBar maxValue={100} currentValue={45} variant={'orange'} />
+              </div>
+              <div className={styles.progressItem}>
+                <p className={styles.progressItemText}>Поднятие ног, согнутых в коленях</p>
+                <ProgressBar maxValue={100} currentValue={45} variant={'purple'} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { Main } from 'pages'
 import { AuthPage } from 'pages/authPage/auth'
+import { Course } from 'pages/course/Course'
 
 export const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Main />} />
     <Route path="/auth" element={<AuthPage />} />
+    <Route path="/course" element={<Course />} />
   </Routes>
 )
